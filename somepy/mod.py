2d8e7d985b9987f98cf9f0e56a6c2cc6366b9253
Kristopher Cooper <kris@infra04-wg012.eduroam.wireless.umn.edu>
2025-10-02 11:10:19 -0500

    mnore testing

--- somepy/mod.py
+++ somepy/mod.py
@@ -4,7 +4,17 @@ def afunc():
     """An example docstring
 
     .. math::
-        (l / (1 + np.exp(-k*(mid_energies.value-x0))) + b)
+        (l / (1 + np.exp(-k*(mid\_energies.value-x0))) + b)
 
     """
     return 1+2
+
+
+
+def bfunc():
+    """An example docstring again
+
+    some more docs here
+
+    """
+    return 3+4
